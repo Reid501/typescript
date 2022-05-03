5266f976e147afe8af601d3a74c8b2f7b7555612
Alex <alexatrs@hotmail.co.uk>
2022-05-03 19:19:49 +0100

    arrays and objects.

--- sandbox.js
+++ sandbox.js
@@ -5,3 +5,22 @@ var circ = function (diameter) {
     return diameter * Math.PI;
 };
 console.log(circ(17));
+// Arrays
+var names = ['Giv', 'Alex', 'Freddie'];
+names.push('Brian');
+// names.push(20);
+var numbers = [10, 20, 30];
+numbers.push(25);
+// numbers.push('test');
+var mixed = ['John', 29, 'Roger', 50];
+mixed.push(60);
+mixed.push('bill');
+// Objects
+var ninja = {
+    name: 'mario',
+    belt: 'black',
+    age: 30
+};
+ninja.age = 40;
+ninja.name = 'yoshi';
+// ninja.age = 'ten';
--- sandbox.ts
+++ sandbox.ts
@@ -7,3 +7,28 @@ const circ = (diameter: number) => {
 }
 
 console.log(circ(17))
+
+// Arrays
+let names = ['Giv', 'Alex', 'Freddie'];
+
+names.push('Brian');
+// names.push(20);
+
+let numbers = [10, 20, 30];
+numbers.push(25);
+// numbers.push('test');
+
+let mixed = ['John', 29, 'Roger', 50];
+mixed.push(60);
+mixed.push('bill');
+
+// Objects
+let ninja = {
+    name: 'mario',
+    belt: 'black',
+    age: 30
+}
+
+ninja.age = 40;
+ninja.name = 'yoshi';
+// ninja.age = 'ten';
